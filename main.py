14f3345508aeed6c8278870dffe33e2785bcc2d3
DelaliFunani <dfunani@live.co.za>
2025-04-17 07:41:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,71 @@
 import json
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from random import choices
-import sys
+from sys import argv
 
 from encryption import Cryptography
 
 PASSWORD_LENGTH = 21
 
-def generate_password_file():
+
+def read_passwords():
     with open("secrets.txt", "r") as file:
         encrypted = file.read().strip()
         decrypted = Cryptography().decrypt(encrypted.encode()).decode()
         passwords = json.loads(decrypted)
-        with open("passwords.json", "w") as file:
-            json.dump(passwords, file)
+        return passwords
 
-def main(length, username, app):
-    with open("password.json", "r") as file:
-        passwords = json.load(file)
-    
-    array = choices(ascii_lowercase + ascii_uppercase + digits + punctuation, k=length)
-    password = "".join(array)
-    
-    if app in passwords:
-        print("Password already exists for this app")
-    else:
-        with open("password.json", "w") as file:
-            passwords.update({app: {"password": password, "username": username}})
-            json.dump(passwords, file)
-    
+
+def remove_password(app: str):
+    with open("secrets.txt", "r") as file:
+        encrypted = file.read().strip()
+        decrypted = Cryptography().decrypt(encrypted.encode()).decode()
+        passwords = json.loads(decrypted)
+
+        try:
+            del passwords[app]
+        except BaseException as e:
+            print(f"Password Could not be Deleted - {str(e)}")
+
+        write_passwords(passwords)
+
+
+def write_passwords(passwords: dict):
     with open("secrets.txt", "w") as file:
         text = json.dumps(passwords)
         encrypted = Cryptography().encrypt(text.encode())
         file.write(encrypted.decode() + "\n")
 
 
-if __name__ == "__main__":
-    argv = sys.argv
+def main(length: int, username: str, app: str):
+    passwords = read_passwords()
 
-    if len(argv) == 2 and argv[1] == "generate":
-        generate_password_file()
+    array = choices(ascii_lowercase + ascii_uppercase + digits + punctuation, k=length)
+    password = "".join(array)
+
+    if app in passwords:
+        print("Password already exists for this app")
+    else:
+        passwords.update({app: {"password": password, "username": username}})
+        print(password)
+
+    write_passwords(passwords)
+
+
+if __name__ == "__main__":
+    if len(argv) == 2 and argv[1] == "all":
+        print(json.dumps(read_passwords(), indent=4))
+    elif len(argv) == 3 and argv[1] == "remove":
+        remove_password(argv[2])
+    elif len(argv) == 3 and argv[1] == "get":
+        print(
+            json.dumps(
+                read_passwords().get(
+                    argv[2], f"Password Could not Be Retrieved - '{argv[2]}'"
+                ),
+                indent=4,
+            )
+        )
     elif len(argv) != 4:
         print("Usage: python main.py [APP] [USERNAME] [PASSWORD_LENGTH]")
     else:
